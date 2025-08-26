61af48a6f99b8820d53a6493eb4d829df82dd262
Luís Felipe Sousa Foresto <luisf.foresto@gmail.com>
2025-08-26 00:49:21 +0000

    codigo finalizado

---
+++ tests/math.test.js
@@ -0,0 +1,13 @@
+const math = require('../math');
+test('soma 2 + 3 igual a 5', () => { 
+expect(math.add(2, 3)).toBe(5); 
+}); 
+test('Subtracao 3 - 2 igual a 1', () => { 
+expect(math.subtract(3, 2)).toBe(1); 
+}); 
+test('Multiplicacao 2 x 3 igual a 6', () => { 
+expect(math.multiply(2, 3)).toBe(5); 
+}); 
+test('Divisao 10 / 2 igual a 5', () => { 
+expect(math.divide(10, 2)).toBe(5); 
+});
