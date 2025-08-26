5333fa30e859d1362d17dd785045bd06779e6274
Luís Felipe Sousa Foresto <luisf.foresto@gmail.com>
2025-08-26 01:07:23 +0000

    Codigo realmente corrigido

--- tests/math.test.js
+++ tests/math.test.js
@@ -6,7 +6,7 @@ test('Subtracao 3 - 2 igual a 1', () => {
 expect(math.subtract(3, 2)).toBe(1); 
 }); 
 test('Multiplicacao 2 x 3 igual a 6', () => { 
-expect(math.multiply(2, 3)).toBe(5); 
+expect(math.multiply(2, 3)).toBe(6); 
 }); 
 test('Divisao 10 / 2 igual a 5', () => { 
 expect(math.divide(10, 2)).toBe(5); 
